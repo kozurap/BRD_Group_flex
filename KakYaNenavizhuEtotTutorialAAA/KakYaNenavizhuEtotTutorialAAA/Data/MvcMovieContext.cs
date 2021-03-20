03a21c86aee94719b91aff5dc6cf9f119f08f2c3
Timur Abdullin <timur.122@yandex.ru>
2021-03-20 12:00:56 +0300

    Deleted HomeController and edited endpoints

---
+++ KakYaNenavizhuEtotTutorialAAA/KakYaNenavizhuEtotTutorialAAA/Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using KakYaNenavizhuEtotTutorialAAA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KakYaNenavizhuEtotTutorialAAA.Data
+{
+    public class MvcMovieContext:DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
